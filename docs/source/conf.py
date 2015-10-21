d7a3e9e2d2fd296fa70146799cacc181318b1342
Roland <rolthiolliere@gmail.com>
2015-10-21 02:49:28 +0200

    removed readthedocs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['argparse', 'scipy', 'numpy', 'pandas', 'h5py', 'h5features']
+MOCK_MODULES = ['numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------

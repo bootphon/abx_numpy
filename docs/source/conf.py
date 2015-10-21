a3cb1089cd990b0661e25f46bf85b69c8af38dd8
Roland <rolthiolliere@gmail.com>
2015-10-21 02:37:36 +0200

    mocked numpy for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,16 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['argparse', 'scipy', 'numpy', 'pandas', 'h5py', 'h5features']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
